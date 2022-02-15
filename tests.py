585dda96575cedd5843f53b5c451721e1e62651d
mylightgarden <mylightgarden@gmail.com>
2022-02-14 21:49:39 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,7 +1,17 @@
+from math import exp
 import unittest
 import task
 
 
 class TestCase(unittest.TestCase):
+    def test1(self):
+        expected = "Hello World"
+        self.assertEqual(task.my_func(), expected)
 
-	pass
+    def test2(self):
+        expected = "Goodbye World"
+        self.assertNotEqual(task.my_func(), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
